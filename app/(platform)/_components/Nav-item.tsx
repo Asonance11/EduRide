e3f92f6141ac4c6471d2baa8e2bef682e62fff42
Oluwadabira <adigunsamuelaso@gmail.com>
2024-02-19 17:54:50 +0100

    styling navbar

--- app/(platform)/_components/Nav-item.tsx
+++ app/(platform)/_components/Nav-item.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { usePathname, useRouter } from 'next/navigation';
-import { CarTaxiFront, History, LayoutDashboard } from 'lucide-react';
+import { CarTaxiFront, History, LayoutDashboard, Settings } from 'lucide-react';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -26,6 +26,11 @@ export default function NavItem() {
       icon: <History className="h-4 w-4 mr-2" />,
       href: '/dashboard/history',
     },
+    {
+      label: 'Settings',
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: '/dashboard/settings',
+    },
   ];
 
   const onClick = (href: string) => {
@@ -41,7 +46,8 @@ export default function NavItem() {
           onClick={() => onClick(route.href)}
           className={cn(
             'w-full font-normal justify-start pl-10 mb-1',
-            pathname === route.href && 'bg-black text-white',
+            pathname === route.href &&
+              'bg-slate-900 text-white hover:bg-slate-900 hover:text-white',
           )}
           variant="ghost"
         >
