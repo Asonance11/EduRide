bce13c575198273f0ac8daa78528dfb6bf481a4a
Oluwadabira <adigunsamuelaso@gmail.com>
2024-02-19 18:49:45 +0100

    dashboard

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import React from 'react';
 import UserInfo from '../_components/UserInfo';
+import { Separator } from '@/components/ui/separator';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -10,6 +11,7 @@ export default function DashBoard() {
   return (
     <div className="w-full mb-20">
       <UserInfo />
+      <Separator className="my-4" />
     </div>
   );
 }
