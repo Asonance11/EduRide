67f12cc01d4ef4b0a3ada8078e64af5d1896f662
Oluwadabira <adigunsamuelaso@gmail.com>
2024-02-29 11:38:36 +0100

    Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,10 +1,10 @@
-import { Loader2 } from 'lucide-react'
-import React from 'react'
+import { Loader2 } from 'lucide-react';
+import React from 'react';
 
 export default function Loader() {
   return (
-    <div className='w-full h-full flex items-center justify-center'>
-        <Loader2 className='animate-spin text-xl'/>
-        </div>
-  )
+    <div className="w-full h-full flex items-center justify-center">
+      <Loader2 className="animate-spin text-xl" />
+    </div>
+  );
 }
