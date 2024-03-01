e40fd3a1873f1cdbf53afbf6e223b4ca5010d9fb
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-01 19:27:40 +0100

    Add user to db with user role

---
+++ actions/createUser.ts
@@ -0,0 +1,21 @@
+import db from '@/lib/db';
+import { currentUser } from '@clerk/nextjs';
+import { UserRole } from '@prisma/client';
+
+export const createUser = async (role: UserRole) => {
+  const loggedUser = await currentUser();
+  if (!loggedUser) {
+    return null;
+  }
+
+  await db.user.create({
+    data: {
+      userId: loggedUser.id,
+      firstname: loggedUser?.firstName!,
+      lastname: loggedUser?.lastName!,
+      imageUrl: loggedUser.imageUrl,
+      email: loggedUser.emailAddresses[0].emailAddress,
+      role: role,
+    },
+  });
+};
