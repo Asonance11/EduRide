2d6de9939871d1a9410296114161b8c9c58cb678
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-01 17:58:28 +0100

    Creae a function that returns user role

---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import { currentUser } from '@clerk/nextjs';
+import db from './db';
+
+export const currentRole = async () => {
+  const loggedUser = await currentUser();
+  if (!loggedUser) {
+    return null;
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      userId: loggedUser.id,
+    },
+  });
+
+  if (user) {
+    return user.role;
+  }
+};
