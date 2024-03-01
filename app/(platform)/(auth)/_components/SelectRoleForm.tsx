4b6034910f95dc3910a836fe9187aba245f79c03
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-01 19:30:49 +0100

    Select role page

---
+++ app/(platform)/(auth)/_components/SelectRoleForm.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { createUser } from '@/actions/createUser';
+import { toast } from 'sonner';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  role: z.enum(['STUDENT', 'DRIVER', 'ADMIN'], {
+    required_error: 'You need to select a role.',
+  }),
+});
+
+export default function SelectRoleForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      await createUser(data.role);
+      toast.success('Account succesfully created');
+      redirect('/dashboard');
+    } catch (error) {
+      console.log(error);
+      toast.error('An error occured');
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="role"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Select your role</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="STUDENT" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Student</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="DRIVER" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Driver</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ app/(platform)/(auth)/select-role/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import SelectRoleForm from '../_components/SelectRoleForm';
+
+export default function SelectRolePage() {
+  return <SelectRoleForm />;
+}
