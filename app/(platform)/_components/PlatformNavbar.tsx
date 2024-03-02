e7ab63c873d5e00fb07fd6ba2741f62bfc0e7f03
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-02 15:30:45 +0100

    Removed useEffect since it does nothing now

--- app/(platform)/_components/PlatformNavbar.tsx
+++ app/(platform)/_components/PlatformNavbar.tsx
@@ -1,27 +1,10 @@
 'use client';
-import { initialProfile } from '@/actions/initialProfile';
 import Logo from '@/components/Logo';
 import { UserButton } from '@clerk/nextjs';
-import { User } from '@prisma/client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import MobileSidebar from './MobileSidebar';
 
 export default function PlatformNavbar() {
-  const [profile, setProfile] = useState<User | null>(null);
-
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const userProfile = await initialProfile();
-        setProfile(userProfile);
-      } catch (error) {
-        console.error('Error fetching profile', error);
-      }
-    };
-
-    fetchProfile();
-  }, []);
-
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
       <MobileSidebar />
