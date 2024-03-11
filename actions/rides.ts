51c878675cca636d8021ccd57ed60c60a3d4b557
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-11 10:07:39 +0100

    Update to book ride page

--- actions/rides.ts
+++ actions/rides.ts
@@ -5,31 +5,53 @@ import db from '@/lib/db';
 import { bookRideFormSchema } from '@/types/schemas';
 import { z } from 'zod';
 import { currentProfile } from './currentProfile';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+async function getUserInfo() {
+  const profile = await currentProfile();
+  const role = await currentRole();
+  return { profile, role };
+}
 
 export const bookRide = async (data: z.infer<typeof bookRideFormSchema>) => {
   try {
-    const profile = await currentProfile();
-    if (!profile) {
+    const { profile, role } = await getUserInfo();
+
+    if (!profile || role !== 'STUDENT') {
       return {
-        error: 'Unauthorized, Log in to continue',
+        error:
+          role !== 'STUDENT'
+            ? 'Only students can book rides'
+            : 'Unauthorized, Log in to continue',
       };
     }
 
-    const role = await currentRole();
+    const existingRide = await db.ride.findFirst({
+      where: {
+        passengerId: profile.id,
+        status: { notIn: ['COMPLETED', 'CANCELLED'] },
+      },
+    });
 
-    if (role !== 'STUDENT') {
+    if (existingRide) {
       return {
-        error: 'Only students can book rides',
+        error:
+          'You have a ride already booked, please complete or cancel it before booking another ride.',
       };
     }
 
-    await db.ride.create({
+    const createdRide = await db.ride.create({
       data: {
         pickupPoint: data.pickupPoint,
         destination: data.destination,
         passengerId: profile.id,
       },
     });
+
+    revalidatePath('/dashboard/available-rides');
+
+    redirect(`/dashboard/ride/${createdRide.id}`);
   } catch (error) {
     throw error;
   }
