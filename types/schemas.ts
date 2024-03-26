32cd6a9f5e85dbba75f8f48abb1b819e44535fc6
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-26 01:00:08 +0100

    idk

--- types/schemas.ts
+++ types/schemas.ts
@@ -4,3 +4,8 @@ export const bookRideFormSchema = z.object({
   pickupPoint: z.string(),
   destination: z.string(),
 });
+
+export const createPlaceFormSchema = z.object({
+  name: z.string(),
+  alias: z.string(),
+});
